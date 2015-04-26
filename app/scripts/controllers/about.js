d39721d3b3c2e562d926686887c7650c464c69c0
unknown <Magiic15@gmail.com>
2015-04-26 18:18:58 +0200

    Contact List Alpha state

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name proyectofinalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the proyectofinalApp
+ */
+angular.module('proyectofinalApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
